72aded09088a6524105adeba7da86fefa7997260
redappleabc <redapple961129@hotmail.com>
2022-09-27 00:19:34 +0900

    remove 127

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const { PingPongServiceClient } = require('./ping_pong_grpc_web_pb');
 const { PingRequest, PongResponse } = require('./ping_pong_pb.js');
 
-var client = new PingPongServiceClient('http://127.0.0.1:8050', null, null);
+var client = new PingPongServiceClient('http://34.123.209.61:8050', null, null);
 
 class App extends Component {
   
